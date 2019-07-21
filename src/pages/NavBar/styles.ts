f17dd8b4412d60f73b80bf12fbea5b6b53cbad41
mgmdi <14-10444@usb.ve>
2019-07-20 23:27:08 -0400

    - Fixed import problem w/ SignUp/styles

---
+++ src/pages/NavBar/styles.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export default useStyles
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -8,8 +8,8 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import useStyles from './styles';
 
 function MadeWithLove() {
   return (
@@ -31,33 +31,6 @@ export interface ISignupProps {
   }
 
 // Estilos 
-
-
-const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
 
 
 // SignUp component
