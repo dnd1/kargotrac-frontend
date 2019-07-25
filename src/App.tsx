8dfa2e4c8b4a04fd24d659a3fe2a274d0bdc7759
mgmdi <14-10444@usb.ve>
2019-07-25 18:57:38 -0400

    testing useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+import { SignUp } from './pages/SignUp';
 import NavBar from './pages/NavBar'
 import userContext from './index'
 
@@ -11,14 +11,12 @@ const App: React.FC = () => {
   const value = useContext(userContext);
   return (
     <Router>
-      {console.log(value)}
       <NavBar />
+
       <Route exact path="/" component={() => <Homepage name="Maria" />} />
       <Route path="/login" component={Login} />
       <Route path="/signup:id" exact component={SignUp} />
       <Route path="/signup" exact component={SignUp} />
-      
-
     </Router>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,22 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import userResponse from './pages/Login'
 
-import { ThemeProvider } from '@material-ui/styles';
-const theme = {};
 
-const userResponse = {
-    "usuario": {
-        "id": 86,
-        "email": "dianar@gmail.com",
-        "username": "diana",
-        "password": "1234567",
-        "address": null,
-        "phone1": null,
-        "phone2": null,
-        "createdAt": "2019-07-24T19:05:58.000Z",
-        "updatedAt": "2019-07-24T19:05:58.000Z"
-    },
-    "company": 1,
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ODYsInVzZXJuYW1lIjoiZGlhbmEiLCJpYXQiOjE1NjQwMTgyODAsImV4cCI6MTU2NDE0Nzg4MH0.WvnZevu7EwTBSHdd4Mhr2diPUj3pieHbtfRMwA1h4Mw"
-  }
-  
-  const userContext = React.createContext(userResponse);
-  export default userContext;
+
+
+const userContext = React.createContext(userResponse);
+export default userContext;
 
 ReactDOM.render(
     // <ThemeProvider theme={theme}>
-        <userContext.Provider value={userResponse}>
-            <App />
-        </userContext.Provider>
-            
+    <userContext.Provider value={userResponse}>
+        <App />
+    </userContext.Provider>
+
     // </ThemeProvider>, 
     , document.getElementById('root')
 );
