da975231d0692353b56dcc6431cacb06e4862801
mgmdi <14-10444@usb.ve>
2019-07-24 22:31:16 -0400

    Se agrego useContext y se uso en App.tsx como ejemplo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import * as React from 'react';
+import React, {useContext} from 'react';
+
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import NavBar from './pages/NavBar'
+import userContext from './index'
 
 const App: React.FC = () => {
+  const value = useContext(userContext);
   return (
     <Router>
-
+      {console.log(value)}
       <NavBar />
       <Route exact path="/" component={() => <Homepage name="Maria" />} />
       <Route path="/login" component={Login} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -7,9 +7,31 @@ import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from '@material-ui/styles';
 const theme = {};
 
+const userResponse = {
+    "usuario": {
+        "id": 86,
+        "email": "dianar@gmail.com",
+        "username": "diana",
+        "password": "1234567",
+        "address": null,
+        "phone1": null,
+        "phone2": null,
+        "createdAt": "2019-07-24T19:05:58.000Z",
+        "updatedAt": "2019-07-24T19:05:58.000Z"
+    },
+    "company": 1,
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ODYsInVzZXJuYW1lIjoiZGlhbmEiLCJpYXQiOjE1NjQwMTgyODAsImV4cCI6MTU2NDE0Nzg4MH0.WvnZevu7EwTBSHdd4Mhr2diPUj3pieHbtfRMwA1h4Mw"
+  }
+  
+  const userContext = React.createContext(userResponse);
+  export default userContext;
+
 ReactDOM.render(
     // <ThemeProvider theme={theme}>
-        <App />
+        <userContext.Provider value={userResponse}>
+            <App />
+        </userContext.Provider>
+            
     // </ThemeProvider>, 
     , document.getElementById('root')
 );
