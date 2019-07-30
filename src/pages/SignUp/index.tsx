146a8549cf4f884280309d52eeed4c5c19215b43
mgmdi <14-10444@usb.ve>
2019-07-29 21:17:28 -0400

    Added text field errors for email and password

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -9,58 +9,48 @@ import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import useStyles from './styles';
 import { Link, withRouter } from "react-router-dom";
 import axios from 'axios'
+import {PopUp} from './popup'
 
-function MadeWithLove() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Built with love by the '}
-      
-      {' team.'}
-    </Typography>
-  );
+
+
+
+
+
+
+// 
+
+const responseHandler = (res: any) => {
+
+  if (res.data.status === "success") {
+
+    window.sessionStorage.setItem("session", res.data.token);
+    window.alert(`
+          El usuario ${res.data.user.email} ha sido registrado
+          El token de sesion es ${res.data.token}
+          Con Status ${res.status}
+          `)
+  } else {
+    console.log("????????????????????????")
+
+
+  }
+
 }
 
-export interface ISignupProps {
-  username: string,
-  email: string,
-  password: string,
-  emailError: boolean,
-  passError: boolean
-}
-
-interface IErrors {
-  emailError: boolean,
-  passError: boolean
-}
-
-
-export const userResponse = {
-  "user": {
-      "id": 86,
-      "email": "dianar@gmail.com",
-      "username": "diana",
-      "password": "1234567",
-      "address": null,
-      "phone1": null,
-      "phone2": null,
-      "createdAt": "2019-07-24T19:05:58.000Z",
-      "updatedAt": "2019-07-24T19:05:58.000Z"
-  },
-  "company": 1,
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ODYsInVzZXJuYW1lIjoiZGlhbmEiLCJpYXQiOjE1NjQwMTgyODAsImV4cCI6MTU2NDE0Nzg4MH0.WvnZevu7EwTBSHdd4Mhr2diPUj3pieHbtfRMwA1h4Mw"
-}
-// Estilos 
-
-
-// SignUp component
 
 export const SignUp = (props: any) => {
   const classes = useStyles();
-  
+  const id = props.match.params && (props.match.params as any).id
 
+  // meter responsehandler aqui y que sea el el que cambie el estado de error
 
   const [signup, setSignup] = React.useState({
     email: "",
@@ -68,135 +58,181 @@ export const SignUp = (props: any) => {
     password: "",
   })
 
-  const [error, setError] = React.useState({
-    emailError : false,
-    passError: false
+  const [emailError, setEmailError] = React.useState({
+    emptyEmail : false,
+    format: false
   })
+
+  const [passError, setPassError] = React.useState(false)
+
+  const [resError, setResError] = React.useState(false)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.persist();
     const { name, value } = e.target
+    console.log(name)
+    console.log(value)
     setSignup({ ...signup, [name]: value })
     console.log(signup)
-    
-    
-    if(name === 'email'){
-      e.target.value === ""? setError({...error, emailError : true}): setError({...error, emailError : false})
-      console.log(error)
-    }else if(name === 'password'){
-      e.target.value === ""? setError({...error, passError : true}): setError({...error, passError : false})
+
+    if (name === 'email') {
+      validate('emailError', e.target.value)
+    } else if (name === 'password') {
+      validate('passError', e.target.value)
     }
-    
+
+  }
+
+
+  const validateEmail = (email : string) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase());
+}
+  const validate = (name : string, value: string) => {
+    let errorVal = false
+    if(value.length === 0) errorVal = true
+    if(name === 'emailError') {
+      setEmailError({...emailError, emptyEmail: errorVal})
+
+      console.log(validateEmail(value))
+    }
+    else setPassError(errorVal)
+    //console.log(error)
+
+  }
+
+  const handleBlur = (e : any) => {
+    setEmailError({...emailError, format: !validateEmail(e.target.value)})
   }
 
   const handleSubmit = (evt: any) => {
     evt.preventDefault();
-    alert(`Submitting Name ${signup.username}`)
+
+
+    setResError(false)
+
+    
+    validate('passError', signup.password)
+    validate('emailError', signup.email)
+
+    
+    
     let user = {
       email: signup.email,
       username: signup.username,
       password: signup.password,
       companyID: props.match.params.id
     }
-    const {email, username, password, companyID} = user
-    console.log(email)
+
     axios.post(`http://localhost:8080/register`, user)
       .then(res => {
         console.log(res);
         console.log(res.data);
         // Hacer siwtch con los casos de errores y adentro del OK esto:
-        if(res.data.status == "success"){
-          window.sessionStorage.setItem("session", res.data.token);
-          window.alert(`
-          El usuario ${res.data.user.email} ha sido registrado
-          El token de sesion es ${res.data.token}
-          Con Status ${res.status}
-          `)
-        }else window.alert(res.data.msg)
+        // Llamar response errors
+        // Cambiar esto a response handler
+        if (res.statusText === "OK") {
+          console.log(res)
+          if(res.data.status === "failed"){
+            setResError(true)
+            console.log("? aqui")
+
+          }
+          
+        } else window.alert(res.data.msg)
       })
   }
-  
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        Es la empresa {props.match.params.id}
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                value={signup.email}
-                onChange={handleChange}
-                error={error.emailError}
-                helperText={error.emailError ? 'Please enter a valid Email' : ' '}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="Uname"
-                value={signup.username}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={signup.password}
-                onChange={handleChange}
-                error={error.passError}
-                helperText={error.passError ? 'Please enter a valid password' : ' '}
-              />
-            </Grid>
 
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              {'Si ya estás registrado con otra empresa de envíos ve al '}
-              <Link to="/login">
-              login
-              </Link>
+  return (
+    <div>
+      {
+        // Cuando reciba el response, de ahi establezco el error y el mensaje de error
+        resError === true ? <PopUp errorMessage="dsdf"></PopUp> : null
+      }
+      
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+      </Typography>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={2} className="container">
+              <Grid item xs={12} >
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={signup.email}
+                  onChange={handleChange}
+                  error={emailError.emptyEmail || emailError.format}
+                  helperText={emailError.emptyEmail || emailError.format ? 'Please enter a valid Email' : ' ' }
+                  onBlur={handleBlur}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="Uname"
+                  value={signup.username}
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value={signup.password}
+                  onChange={handleChange}
+                  error={passError}
+                  helperText={passError ? 'Please enter a valid password' : ' '}
+                />
+              </Grid>
+
             </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={5}>
-        <MadeWithLove />
-      </Box>
-    </Container>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up
+        </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                {'Si ya estás registrado con otra empresa de envíos ve al '}
+                <Link to={`/login${id}`}>
+                  login
+            </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={5}>
+
+        </Box>
+      </Container>
+    </div>
+
   );
 }
 withRouter(SignUp)
