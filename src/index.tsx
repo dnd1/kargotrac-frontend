9482bd494e81d364e1684d485d5f057a989990ca
mgmdi <14-10444@usb.ve>
2019-08-12 14:56:49 -0400

    Added muitheme to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,30 @@
 import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './App';
+import { App } from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#0097a7',
+        },
+        secondary: {
+            main: '#ff5722',
+        },
+    },
 
-
-
-
-
+});
 //export default userContext;
 
 ReactDOM.render(
     // <ThemeProvider theme={theme}>
+    <MuiThemeProvider theme={theme}>
         <App />
 
+    </MuiThemeProvider>
 
     // </ThemeProvider>, 
     , document.getElementById('root')
